042c0efbfccc75032ffca20b54aac9212d5d4dd1
Poseidon5000 <okonnsikangoodnews@gmail.com>
2023-02-13 11:20:55 +0100

    loops class

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+
+// let a = 2 + '2;'
+// let b = 2 + 2;
+// let c = '2' + 2;
+// let d = '2' + '2';
+// let e = 22 - '22'
+
+
+// // console.log(a, b, c, d, e);
+
+// const age = 100
+
+// const friends = ['mitchy', 'ediomo', 'uyai', 'tony', 'tukkie'];
+
+// const name = ['Goodnews', 'Okon', age, friends]
+
+
+// console.log(name);
+
+// console.log(
+//     name[3][0]
+// );
+
+
+
+//EQUALITY OPERATORS (==, ===)
+
+//LOGICAL OPERATORS (||, &&)
+
+// let myFirstLang = "python";
+
+// let myFirstFramework = "next";
+
+// let myBackendLang = "node"
+
+// let myBackFramework = "express"
+
+// const myYears = 18;
+
+// if (myFirstLang == 'python' && myBackendLang == "node" || myFirstFramework == "react") {
+//     console.log("yes this is lang");
+// }
+// else{
+//     console.log("no it isnt");
+// }
+
+
+//OBJECTS
+
+
+const myFavCourses = ["anatomy", "physiology", "biochem", "pharm"];
+
+myFavCourses[1] = "stats"
+
+console.log(myFavCourses);
+
+
+const infoOfPoseidon = {
+    name: "Dara-sifon",
+    surname: "Okon",
+    course: "Human Anatomy",
+    programmingLang: "javascript",
+    years: 2,
+    certifications: true
+}
+
+// console.log(infoOfPoseidon.certifications, infoOfPoseidon.name);
