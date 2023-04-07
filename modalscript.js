53a04ab3ff87096ecf4e84f031773a580f9fc5b2
Poseidon5000 <okonnsikangoodnews@gmail.com>
2023-04-07 22:09:42 +0200

    ben's class

--- modalscript.js
+++ modalscript.js
@@ -45,3 +45,67 @@ document.addEventListener("keydown", function(e){
       console.log("this is not the escape key")
     }
 })
+
+
+
+
+
+
+
+
+
+
+//  let buttons = document.querySelectorAll(".show-modal");
+//  let hiddenBox = document.querySelector(".modal");
+//  let blurBackground  = document.querySelector(".overlay");
+// let body = document.querySelector("body")
+// let close = document.querySelector(".close-modal")
+
+
+// // buttons.addEventListener("click", function(){
+// //   console.log("hello universe");  
+// // })
+// // console.log("hello world");
+
+// for(i=0; i <= buttons.length-1; i++){
+    
+//     buttons[i].addEventListener("click", function(){
+//         // modal.classList.remove("hidden");
+//         // overlay.classList.remove("hidden");
+//         // body.style.background = "red"
+
+//         hiddenBox.classList.remove("hidden");
+//         blurBackground.classList.remove("hidden")
+//         body.style.background = "white"
+//     })
+// }
+
+// close.addEventListener("click", function(){
+//         hiddenBox.classList.add("hidden");
+//         blurBackground.classList.add("hidden")
+//          body.style.background = "linear-gradient(to top left, #300e6e, #2c7b8f)"
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
